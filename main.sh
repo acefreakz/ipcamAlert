6deb43bce44014744c7cd18f5a13cdc0478fd795
Shoo Kyuk Wei <acefreakz@gmail.com>
2016-10-22 13:36:41 +0800

    Bug fix: Fix incorrect behavior if currentLastFilename is empty! (If somehow the log is deleted...)

--- main.sh
+++ main.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
 
-
 function play_alert_sound {
 	mplayer intruder_alarm.mp3
 }
@@ -9,33 +8,31 @@ function update_lastFilename {
 	echo $1 > .lastRecordedFilename
 }
 
-        
+
+# TODO [20161020] Cater for multiple IPCAMs
 while [ true ]; do
-	# Get the last line of log from /var/log/vsftpd.log
-	# Sample: lastline=Thu Oct 20 18:01:26 2016 [pid 10174] [iptvuser] OK UPLOAD: Client "192.168.0.7", "/files/20161020/images/A16102018012410.jpg", 142103 bytes, 375.94Kbyte/sec
-	lastline=`grep "OK UPLOAD: Client \"192.168.0.7\"" /var/log/vsftpd.log | tail -n 1`
-	echo "DEBUG: lastline=$lastline"
+	if [ ! -f .lastRecordedFilename ]; then
+		echo > .lastRecordedFilename
+	fi
 
 	# Get the filename
 	# NOTE: The filename start with a 'A' indicating that this is an ALARM generated image file
-	currentLastFilename=`echo $lastline | grep -oE 'A[0-9]*\.jpg'`
+	# Sample: lastline=Thu Oct 20 18:01:26 2016 [pid 10174] [iptvuser] OK UPLOAD: Client "192.168.0.7", "/files/20161020/images/A16102018012410.jpg", 142103 bytes, 375.94Kbyte/sec
+	# Captured: /files/20161020/images/A16102018012410.jpg
+	currentLastFilename=`grep "\[iptvuser\] OK UPLOAD: Client \"192.168.0.7\"" /var/log/vsftpd.log | grep -oE "/files.*A[0-9]*\.jpg" | tail -n 1`
 	echo "DEBUG: currentLastFilename=$currentLastFilename"
-	
-	# Retrieve last recorded filename
-	if [ -f .lastRecordedFilename ]; then
+
+	# Compare filenames. If different, sound the alarm!
+	if [ -f .lastRecordedFilename ] && [ ! -z $currentLastFilename ]; then
 		if [ "`cat .lastRecordedFilename`" == "$currentLastFilename" ]; then
 			echo "DEBUG: Filename matched, no need to arm the alarm!"
 		else
 			echo "DEBUG: New alarm filename! Update to file '.lastRecordedFilename'. Sound the siren now!"
+			echo $(date +"%T") "Alert!" >> log.tmp
 			update_lastFilename $currentLastFilename
 			play_alert_sound
 		fi
-	else
-		echo "DEBUG: .lastRecordedFilename not found! Write to file '.lastRecordedFilename'. Sound the siren now!"
-		update_lastFilename $currentLastFilename
-		play_alert_sound
 	fi
 
-	echo "sleeping for 5 seconds..."	
 	sleep 5
 done
